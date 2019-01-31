c7115221ee471941aed951704b9ceb2b51a1e8b1
rodrigo oliveira <rodrigo.soliveira2322@gmail.com>
2019-01-31 21:07:53 -0200

    Retirada senhas do appsettings

--- MeuTrabalho/Controllers/AccountController.cs
+++ MeuTrabalho/Controllers/AccountController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MeuTrabalho.Models;
 using Microsoft.AspNetCore.Mvc;
-using MeuTrabalho.Models;
+using System;
 using System.Data.SqlClient;
 
 namespace MeuTrabalho.Controllers
--- MeuTrabalho/Startup.cs
+++ MeuTrabalho/Startup.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,13 +14,11 @@ namespace MeuTrabalho
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
